82d2977cdc140844aa022fb9827e173c74ec2a08
brijhaywood <briyanahaywood@gmail.com>
2021-08-12 23:26:53 -0500

    Registered place to admin.py

--- capstone/facts/admin.py
+++ capstone/facts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+@admin.register(models.Place)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'year', 'coord_v', 'coord_h', 'is_liked', 'has_viewed', 'category')
+    # prepopulated_fields = {'slug': ('title',), }
